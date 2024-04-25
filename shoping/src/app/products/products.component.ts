cd331ea11972141bf2e3165661961a5423d8eec0
negardev92 <121099957+negardev92@users.noreply.github.com>
2024-04-25 21:24:37 +0330

    fix shoping cart&&send deta

--- shoping/src/app/products/products.component.ts
+++ shoping/src/app/products/products.component.ts
@@ -10,23 +10,24 @@ import{ProductapiService} from '../productapi.service';
 export class ProductsComponent implements OnInit {
   products: any[];
   userbasket= [];
-  items =[] ;
-
-   
   
+ 
   constructor(private productapiService:ProductapiService,) { }
   
   ngOnInit(): void {
 
     this.productapiService.getData().subscribe((data) => {this.products = data});
   }
-
+ 
   
  getId(event){
   let item = this.products.find(i => i.id === event.id);
   if(item != null){
-    this.userbasket.push(item);
+      this.userbasket.push(item);
+    console.log(this.userbasket)
+    
   }
+
 
   
  }
--- shoping/src/app/shoping-cart/shoping-cart.component.ts
+++ shoping/src/app/shoping-cart/shoping-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shoping-cart',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-cart.component.css']
 })
 export class ShopingCartComponent implements OnInit {
-productitem: any;
 
+@Input() list:[];
   constructor() { }
 
   ngOnInit(): void {
+     
   }
+  
 
 }
